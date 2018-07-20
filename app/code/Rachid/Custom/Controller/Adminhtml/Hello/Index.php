19c11c35a06995199d042886eb245824a4b88e57
rnaaim <rnaaim@DEGETEL-L0228.oceanie.intranet.degetel.com>
2018-07-20 16:13:58 +0200

    Creatings submenu Import inside Customers

---
+++ app/code/Rachid/Custom/Controller/Adminhtml/Hello/Index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rnaaim
+ * Date: 20/07/2018
+ * Time: 15:31
+ */
+
+namespace Rachid\Custom\Controller\Adminhtml\Hello;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return  $resultPage = $this->resultPageFactory->create();
+    }
+}
+
---
+++ app/code/Rachid/Custom/Registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rnaaim
+ */
+
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Rachid_Custom', __DIR__);
