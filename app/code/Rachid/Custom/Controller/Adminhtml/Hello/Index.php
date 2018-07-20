4e136bbcb2af3ae29427e1ce3ed6925b2b929db6
rnaaim <rnaaim@DEGETEL-L0228.oceanie.intranet.degetel.com>
2018-07-20 17:41:36 +0200

    Add bulk insert of customers

--- app/code/Rachid/Custom/Controller/Adminhtml/Hello/Index.php
+++ app/code/Rachid/Custom/Controller/Adminhtml/Hello/Index.php
@@ -7,6 +7,9 @@
  */
 
 namespace Rachid\Custom\Controller\Adminhtml\Hello;
+use \Magento\Framework\Registry;
+use \Magento\Framework\App\Bootstrap;
+include('app/bootstrap.php');
 
 class Index extends \Magento\Backend\App\Action
 {
@@ -35,7 +38,45 @@ class Index extends \Magento\Backend\App\Action
      */
     public function execute()
     {
-        return  $resultPage = $this->resultPageFactory->create();
+        $users = $this->getCustomers();
+
+        foreach ($users as $key => $user){
+
+            $bootstrap = Bootstrap::create(BP, $_SERVER);
+            $objectManager = $bootstrap->getObjectManager();
+            $url = \Magento\Framework\App\ObjectManager::getInstance();
+            $storeManager = $url->get('\Magento\Store\Model\StoreManagerInterface');
+            $mediaurl= $storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+            $state = $objectManager->get('\Magento\Framework\App\State');
+            $state->setAreaCode('frontend');
+
+            $customerFactory = $objectManager->get('\Magento\Customer\Model\CustomerFactory');
+            $websiteId = $storeManager->getWebsite()->getWebsiteId();
+            $store = $storeManager->getStore();
+            $storeId = $store->getStoreId();
+            
+            $customer = $customerFactory->create();
+            $customer->setWebsiteId($websiteId);
+
+            $customer->setId($user->id);
+            $customer->setEmail($user->first_name."@degetel.com");
+            $customer->setFirstname($user->first_name);
+            $customer->setLastname($user->last_name);
+            $customer->setPassword($user->first_name."123");
+
+                $customer->save();
+        }
+        return $resultPage = $this->resultPageFactory->create();
+    }
+
+
+    public function getCustomers(){
+
+            $users_json = file_get_contents('https://reqres.in/api/users');
+
+            $users = json_decode($users_json);
+
+            return $users->data;
     }
 }
 
