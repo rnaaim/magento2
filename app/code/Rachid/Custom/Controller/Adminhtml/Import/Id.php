db693ba844979e8ec0b6c4c1d24fecd9a0b84d97
rnaaim <rnaaim@DEGETEL-L0228.oceanie.intranet.degetel.com>
2018-07-24 10:49:03 +0200

    Add import by Id

---
+++ app/code/Rachid/Custom/Controller/Adminhtml/Import/Id.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Rachid\Custom\Controller\Adminhtml\Import;
+use \Magento\Framework\Registry;
+use \Magento\Framework\App\Bootstrap;
+include('app/bootstrap.php');
+
+class Id extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {   $user = $this->getACustomer();
+
+
+           $bootstrap = Bootstrap::create(BP, $_SERVER);
+            $objectManager = $bootstrap->getObjectManager();
+            $url = \Magento\Framework\App\ObjectManager::getInstance();
+            $storeManager = $url->get('\Magento\Store\Model\StoreManagerInterface');
+            $mediaurl= $storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
+            $state = $objectManager->get('\Magento\Framework\App\State');
+            $state->setAreaCode('frontend');
+
+            $customerFactory = $objectManager->get('\Magento\Customer\Model\CustomerFactory');
+            $websiteId = $storeManager->getWebsite()->getWebsiteId();
+            $store = $storeManager->getStore();
+            $storeId = $store->getStoreId();
+
+            $customer = $customerFactory->create();
+            $customer->setWebsiteId($websiteId);
+
+            $customer->setId($user->id);
+            $customer->setEmail($user->first_name."@degetel.com");
+            $customer->setFirstname($user->first_name);
+            $customer->setLastname($user->last_name);
+            $customer->setPassword($user->first_name."123");
+
+            $customer->save();
+
+        return $resultPage = $this->resultPageFactory->create();
+    }
+
+
+    public function getACustomer(){
+
+        $id = $_POST["id"];
+        $user_json = file_get_contents('https://reqres.in/api/users/'.$id);
+
+        $user = json_decode($user_json);
+
+        return $user->data;
+    }
+}
+
---
+++ app/code/Rachid/Custom/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rachid\Custom\Controller\Adminhtml\Index;
+use \Magento\Framework\Registry;
+use \Magento\Framework\App\Bootstrap;
+include('app/bootstrap.php');
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+    protected $urlBuilder;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $resultPage = $this->resultPageFactory->create();
+
+    }
+
+}
+
